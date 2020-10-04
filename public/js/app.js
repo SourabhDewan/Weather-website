64094d54dbac1e75fe5d8cb5308b4f77018d0543
Sourabh Dewan <dewan.sourabh28@gmail.com>
2020-10-04 18:00:10 +0530

    Setup aap for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) =>{
     first.textContent = 'Loading...'
     second.textContent= ''
         
-    fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
+    fetch('/weather?address=' + location).then((response) =>{
         response.json().then((data) =>{
             if (data.error) {
                 first.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast = require('../../weather-app/utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000 
+
 // Define paths for express configration 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../template/views')
@@ -100,6 +102,6 @@ app.get('*', (req, res) =>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port + '.')
 })
